74638039fc37c1d027ff0af63ee139cec344efab
Samet Çağrı AKTEPE <blackorsaktepe@gmail.com>
2022-03-19 13:12:50 +0300

    Get Different Types of Data from the User: Basic Input Types

--- form.php
+++ form.php
@@ -8,8 +8,45 @@
 </head>
 <body>
     <form method="post" action="process_form.php">
-        <input name="search" type="text" id="search">
-        <input type="password" name="password" id="password">
+        <div>
+            text: <input type="text">
+        </div>
+        <div>
+            password: <input type="password">
+        </div>
+        <div>
+            tel: <input type="tel">
+        </div>
+        <div>
+            url: <input type="url">
+        </div>
+        <div>
+            date: <input type="date">
+        </div>
+        <div>
+            time: <input type="time">
+        </div>
+        <div>
+            week: <input type="week">
+        </div>
+        <div>
+            color: <input type="color">
+        </div>
+        <div>
+            email: <input type="email">
+        </div>
+        <div>
+            month: <input type="month">
+        </div>
+        <div>
+            range: <input type="range">
+        </div>
+        <div>
+            hidden: <input name="hidden" type="hidden">
+        </div>
+        <div>
+            number: <input name="number" type="number">
+        </div>
         <button>Send</button>
     </form>
 </body>
