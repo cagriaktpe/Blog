ee4884c908ce939be1a010f24ca2b930e53e730a
Samet Çağrı AKTEPE <blackorsaktepe@gmail.com>
2022-03-20 14:46:58 +0300

    Present a Fixed List of Options: the Select Element

--- form.php
+++ form.php
@@ -8,12 +8,18 @@
 </head>
 <body>
     <form method="post" action="process_form.php">
-        <div>
-            text: <input type="text" name="text">
-        </div>
-        <div>
-            textarea: <textarea name="content" rows="7" col="50">Placeholder</textarea>
-        </div>
+        <select name="country" id="country">
+            <optgroup label="Europe">
+                <option value="germany">Germany</option>
+                <option value="france">France</option>
+                <option value="uk">United Kingdom</option>
+            </optgroup>
+            <optgroup label="America">
+                <option value="brazil" selected>Brazil</option>
+                <option value="canada">Canada</option>
+                <option value="usa">United States</option>
+            </optgroup>
+        </select>
         <button>Send</button>
     </form>
 </body>
