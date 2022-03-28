47ba2cdd82f484c36dea6865666b8d367701cc65
Samet Çağrı AKTEPE <blackorsaktepe@gmail.com>
2022-03-28 14:28:38 +0300

    Validate the Form Data and redisplay the Form with Error Messages if Invalid

--- new_article.php
+++ new_article.php
@@ -6,32 +6,53 @@
     */
     require './includes/database.php';
 
+    $errors = [];
+
     if($_SERVER["REQUEST_METHOD"] == "POST") {
 
-        $conn = getDB();
-
-        $sql = "INSERT INTO article (title, content, published_at)
-                VALUES (?, ?, ?)"; 
-        
-        $stmt = mysqli_prepare($conn, $sql);
-
-        if ($stmt === false) {
-            echo mysqli_error($conn);
-        } else {
-            mysqli_stmt_bind_param($stmt, "sss", $_POST['title'], $_POST['content'], $_POST['published_at']);
-            
-            if(mysqli_stmt_execute($stmt)) {
-                $id = mysqli_insert_id($conn);
-                echo "Inserted record with ID: $id";
-            } else {
-                echo mysqli_stmt_error($stmt);
-            }
+        if($_POST['title'] == '') {
+            $errors[] = 'Title is required';
         }
+        if($_POST['content'] == '') {
+            $errors[] = 'Content is required';
+        }
+
+        if(empty($errors)) {
+            $conn = getDB();
+
+            $sql = "INSERT INTO article (title, content, published_at)
+                    VALUES (?, ?, ?)"; 
+        
+            $stmt = mysqli_prepare($conn, $sql);
+
+            if ($stmt === false) {
+                echo mysqli_error($conn);
+            } else {
+                mysqli_stmt_bind_param($stmt, "sss", $_POST['title'], $_POST['content'], $_POST['published_at']);
+            
+                if(mysqli_stmt_execute($stmt)) {
+                    $id = mysqli_insert_id($conn);
+                    echo "Inserted record with ID: $id";
+                } else {
+                    echo mysqli_stmt_error($stmt);
+                }
+            }
+        } 
     }
 ?>
 <?php require 'includes/header.php'; ?>
 
 <h2>New Article</h2>
+
+<?php if (! empty($errors)): ?>
+    <ul>
+    <?php foreach ($errors as $error): ?>
+        <li>
+            <?= $error ?>
+        </li>
+    <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
 
 <form method="post">
     <div>
